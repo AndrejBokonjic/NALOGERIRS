0dc11f43791efb4a4444a903f5c98662f6081c4e
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2024-06-05 22:13:46 +0200

    Popravki za boljše branje novih pdf-jev Butterfly test - druge vrste
    Še vedno je potrebnih nekaj nadaljnjih popravkov, da se lahko brezhibno berejo.
    
    V drugem tipu pdf-ja iz Butterfly testa je nekje mm in nekje stopinje. Treba je razmisliti in vprašati, saj je to lahko problem za modelu.

--- python/pdfReadPlumber.py
+++ python/pdfReadPlumber.py
@@ -5,29 +5,33 @@ import numpy as np
 import re
 import sys
 
+
 def clean_cell(cell):
     """Remove the values inside parentheses from a cell"""
     if cell is None:
         return cell
     return re.sub(r'\(.*?\)', '', cell).strip()
 
+
 def remove_signs(cell):
     """Remove % and mm strings from the cell"""
     if cell is None:
         return cell
     cell = re.sub(r'%', '', cell)
+    cell = re.sub(r'°', '', cell)
     cell = re.sub(r'\bmm\b', '', cell)
     return cell.strip()
+
 
 def extract_text_from_pdf(file_path, include_null_tables=False):
     tables = []
     text_data = []
-    excluded_headers = ["SmoothnessofMovements", "Movement plots"]
+    excluded_headers = ["SmoothnessofMovements", "Movement plots", "Movement Plots"]
     graphical_results_variations = [
-        "Graphical Results", "GRAPHICAL RESULTS", "Graphical results", "graphical results", 
-        "GraphicalResults", "GRAPHICALRESULTS", "Graphicalresults", "graphicalresults"
+        "Graphical Results", "GRAPHICAL RESULTS", "Graphical results", "graphical results",
+        "GraphicalResults", "GRAPHICALRESULTS", "Graphicalresults", "graphicalresults", "Graphic Results"
     ]
-    
+
     # Extract text from the entire PDF to check for "Butterfly-method"
     full_text = ""
 
@@ -37,8 +41,7 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
             full_text += text
             text_data.append(f"Page {page.page_number} Text:\n{text}\n" + "-" * 80)
 
-
-    if "Butterfly-method" in full_text:
+    if "Butterfly-method" in full_text or 'The Butterfly Test Report' in full_text:
         # Execute code for Butterfly-method PDFs
 
         with pdfplumber.open(file_path) as pdf:
@@ -48,7 +51,7 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                     df = pd.DataFrame(table[1:], columns=table[0])
                     df = df.replace({np.nan: None})
                     columns = df.columns.tolist()
-                    
+
                     # Check if the table should be excluded
                     if any(header in columns for header in excluded_headers):
                         continue
@@ -61,22 +64,26 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                     # Check if the table has a header matching any of the variations
                     if any(header in columns for header in graphical_results_variations):
                         # Find the index of the matching header
-                        header_index = next((columns.index(header) for header in graphical_results_variations if header in columns), None)
+                        header_index = next(
+                            (columns.index(header) for header in graphical_results_variations if header in columns),
+                            None)
                         if header_index is not None:
-                            df = df.drop(index=range(header_index, header_index + 3))  # Delete the first three rows after the header
+                            df = df.drop(index=range(header_index,
+                                                     header_index + 3))  # Delete the first three rows after the header
                             df = df.iloc[:-2]  # Delete the last two rows of the entire table
-                    
+
                     # Find the indices of all cells containing the string "2SD"
-                    sd_indices = [(i, j) for i in range(len(df)) for j in range(len(df.columns)) if df.iloc[i, j] == '2SD']
+                    sd_indices = [(i, j) for i in range(len(df)) for j in range(len(df.columns)) if
+                                  df.iloc[i, j] == '2SD']
 
                     # Delete cells directly below each "2SD" cell
                     for i, j in sd_indices:
-                        for k in range(i, len(df)): 
+                        for k in range(i, len(df)):
                             df.iloc[k, j] = None
 
                     # Delete rows with None values in '2SD' columns
                     for i, j in sd_indices:
-                        for k in range(i, len(df)): 
+                        for k in range(i, len(df)):
                             df.iloc[k, j] = None
 
                     # Drop rows and columns where all values are None
@@ -90,7 +97,12 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                     # Replace values in cells containing "Client performance" or "Average distance" with "-"
                     for i in range(len(table_data)):
                         for j in range(len(table_data[i])):
-                            if "Client performance" in str(table_data[i][j]) or "Average distance" in str(table_data[i][j]):
+
+                            if "Patient performance" in str(table_data[i][j]) or "Average Amplitude Accuracy" in str(
+                                    table_data[i][j]):
+                                table_data[i][j] = "-"
+                            if 'Client performance' in str(table_data[i][j]) or 'Average distance' in str(
+                                    table_data[i][j]):
                                 table_data[i][j] = "-"
 
                     # Replace cells containing only "-" with an empty string
@@ -100,13 +112,14 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                                 table_data[i][j] = ""
 
                     # Move data after "%" sign to the next cell
+                    # !!!!!!!!!!!!!!!!!!!!!!!
                     for i in range(len(table_data)):
                         for j in range(len(table_data[i])):
                             if "%" in str(table_data[i][j]):
                                 index = str(table_data[i][j]).index("%")
                                 if index < len(str(table_data[i][j])) - 1:
                                     table_data[i][j + 1] = str(table_data[i][j])[index + 1:].strip()
-                                    table_data[i][j] = str(table_data[i][j])[:index+1].strip()
+                                    table_data[i][j] = str(table_data[i][j])[:index + 1].strip()
 
                     # Apply the remove_signs function to each cell
                     for i in range(len(table_data)):
@@ -128,7 +141,8 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
 
                         if 2 * value_in_cell > empty_or_none_cell:
                             # Remove rows with all empty cells
-                            final_table = [row for row in table_data if not all(cell == "" or cell is None for cell in row)]
+                            final_table = [row for row in table_data if
+                                           not all(cell == "" or cell is None for cell in row)]
                         else:
                             final_table = []
 
@@ -146,7 +160,7 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                     df = pd.DataFrame(table[1:], columns=table[0])
                     df = df.replace({np.nan: None})
                     columns = df.columns.tolist()
-                    
+
                     # Check if the table should be excluded
                     if any(header in columns for header in excluded_headers):
                         continue
@@ -158,22 +172,26 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                     # Check if the table has a header matching any of the variations
                     if any(header in columns for header in graphical_results_variations):
                         # Find the index of the matching header
-                        header_index = next((columns.index(header) for header in graphical_results_variations if header in columns), None)
+                        header_index = next(
+                            (columns.index(header) for header in graphical_results_variations if header in columns),
+                            None)
                         if header_index is not None:
-                            df = df.drop(index=range(header_index, header_index + 3))  # Delete the first three rows after the header
+                            df = df.drop(index=range(header_index,
+                                                     header_index + 3))  # Delete the first three rows after the header
                             df = df.iloc[:-2]  # Delete the last two rows of the entire table
-                    
+
                     # Find the indices of all cells containing the string "2SD"
-                    sd_indices = [(i, j) for i in range(len(df)) for j in range(len(df.columns)) if df.iloc[i, j] == '2SD']
+                    sd_indices = [(i, j) for i in range(len(df)) for j in range(len(df.columns)) if
+                                  df.iloc[i, j] == '2SD']
 
                     # Delete cells directly below each "2SD" cell
                     for i, j in sd_indices:
-                        for k in range(i, len(df)): 
+                        for k in range(i, len(df)):
                             df.iloc[k, j] = None
 
                     # Delete rows with None values in '2SD' columns
                     for i, j in sd_indices:
-                        for k in range(i, len(df)): 
+                        for k in range(i, len(df)):
                             df.iloc[k, j] = None
 
                     # Drop rows and columns where all values are None
@@ -192,7 +210,7 @@ def extract_text_from_pdf(file_path, include_null_tables=False):
                             else:
                                 value_in_cell += 1
 
-                    if 2*value_in_cell > empty_or_none_cell:
+                    if 2 * value_in_cell > empty_or_none_cell:
                         final_table = [row for row in table_data if not all(cell == "" or cell is None for cell in row)]
                         tables.append(final_table)
 
